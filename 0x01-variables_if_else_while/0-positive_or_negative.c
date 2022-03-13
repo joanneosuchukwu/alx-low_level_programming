3c5f7887ae484a2b43a4c2bd115e7f856031e356
joanneosuchukwu <67681741+joanneosuchukwu@users.noreply.github.com>
2022-03-14 00:12:27 +0100

    edited 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - print positive, negative, and zero depending on the number
  * Description: this will program prints "positive, negative, and zero"
- * Returns: Always 0 (Success)
+ * Return: 0
  */
 int main(void)
 {
