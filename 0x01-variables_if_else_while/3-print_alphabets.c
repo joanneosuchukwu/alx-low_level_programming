8419d80a2da3cf36272708b22023806bef73b151
joanneosuchukwu <67681741+joanneosuchukwu@users.noreply.github.com>
2022-03-14 23:27:04 +0100

    added answer to 3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - for ch less than or equal to z putchar ch
+ * Description: uses putchar to print to standard output
+ * Return: void
+ */
+int main(void)
+{
+        int ch;
+	int capitalch;
+
+        for (ch = 'a'; ch <= 'z'; ch++)
+        putchar(ch);
+	for (capitalch = 'A'; capitalch <= 'Z'; capitalch++)
+	putchar(capitalch);
+        putchar('\n');
+        return (0);
+}
