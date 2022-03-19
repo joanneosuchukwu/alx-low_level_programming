28289c8340665680805bf79d4993c2cd9e7636e0
joanneosuchukwu <67681741+joanneosuchukwu@users.noreply.github.com>
2022-03-19 14:43:20 +0100

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - program prints _putchar followed by new line
+ *
+ * Return (0)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return(0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+read me 0x02-functions_nested_loops
